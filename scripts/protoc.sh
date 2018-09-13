29598c348094fdc8e92345049757cba61d993ef0
christian <christian.rivasseau@gmail.com>
2018-09-13 08:55:25 +0200

    adds submodules

--- scripts/protoc.sh
+++ scripts/protoc.sh
@@ -25,3 +25,22 @@ GRPC_JAVA_COMPILER=/opt/grpc-java/compiler/build/exe/java_plugin/protoc-gen-grpc
     --proto_path .                                           \
     --proto_path /opt/grpc/src/proto                         \
     grpc/status/status.proto
+
+generate_google() {
+/usr/local/bin/protoc --proto_path=.                  \
+    --proto_path third_party/googleapis               \
+    --cpp_out=.                                       \
+    --grpc_out=.                                      \
+    --plugin=protoc-gen-grpc=`which grpc_cpp_plugin`  \
+    $1
+}
+
+generate_google google/api/annotations.proto
+generate_google google/api/http.proto
+generate_google google/rpc/status.proto
+generate_google google/type/latlng.proto
+generate_google google/firestore/v1beta1/common.proto
+generate_google google/firestore/v1beta1/document.proto
+generate_google google/firestore/v1beta1/firestore.proto
+generate_google google/firestore/v1beta1/query.proto
+generate_google google/firestore/v1beta1/write.proto
--- tortuga/modules/firestore.cc
+++ tortuga/modules/firestore.cc
@@ -1,5 +1,7 @@
 #include "tortuga/modules/firestore.h"
 
+#include "folly/dynamic.h"
+#include "folly/json.h"
 #include "gflags/gflags.h"
 #include "glog/logging.h"
 
@@ -7,6 +9,8 @@ DEFINE_string(firestore_project, "", "firestore project to update.");
 DEFINE_string(firestore_collection, "", "firestore collection under which to update tasks.");
 
 namespace tortuga {
+using folly::dynamic;
+
 FirestoreModule::FirestoreModule(HttpClient* http) : http_(http) {
   CHECK(http != nullptr);
 }
@@ -16,17 +20,33 @@ FirestoreModule::~FirestoreModule() {
 
 void FirestoreModule::OnProgressUpdate(const TaskProgress& task) {
   std::string url = "https://firestore.googleapis.com/v1beta1/projects/"
-      + FLAGS_firestore_project + "/databases/(default)/documents/
-      + "" tortugas/32"
+      + FLAGS_firestore_project
+      + "/databases/(default)/documents/"
+      + FLAGS_firestore_collection
+      + "/"
+      + task.handle();
 
   std::map<std::string, std::string> headers;
   headers["Content-Type"] = "application/json";
 
-  http_->Patch();
+  dynamic fields = dynamic::object;
+  fields["uuid"] = dynamic::object("stringValue", task.handle());
 
-curl -X PATCH \
-     -H "Content-Type: application/json" \
-     -d '{"fields" : { "handle" : { "stringValue" : "handle_2" } }}' \
-     'https://firestore.googleapis.com/v1beta1/projects/leftyv2-1136/databases/(default)/documents/tortugas/32'
+  // Map<String, Object> token = new HashMap<>();
+  // token.put("uuid", id);
+  // token.put("done", true);
+  // token.put("progress", 1.0d);
+  //  token.put("done_date", new Date());
+  //  token.put("status_code", status.getCode().value());
+  //  if (!Strings.isNullOrEmpty(status.getDescription())) {
+  //    token.put("status_description", status.getDescription());
+  //  }
+
+
+  folly::dynamic body = folly::dynamic::object;
+  
+  
+
+  http_->Patch(url, headers, "");
 }
 }  // namespace tortuga
