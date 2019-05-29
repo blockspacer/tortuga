97720f71b6a37bda6f1d378ec24b36f74535852c
Christian <christian@lefty.io>
2019-05-29 18:41:24 +0200

    sdf

---
+++ tortuga/storage/fields.cc
@@ -0,0 +1,8 @@
+#include "tortuga/storage/fields.h"
+
+namespace tortuga {
+const int TasksFields::kWorkerUuid = 10;
+
+const int WorkersFields::kWorkerId = 0;
+const int WorkersFields::kUuid = 1;
+}  // namespace tortuga
---
+++ tortuga/storage/fields.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace tortuga {
+class TasksFields {
+ public:
+  static const int kWorkerUuid;
+};
+
+class WorkersFields {
+ public:
+  static const int kUuid;
+  static const int kWorkerId;
+};
+}  // namespace tortuga
